57e855baebe775ef230a57b49f032695eb3f5253
zdeni sykorova <sykorova@hotmail.com>
2025-04-20 09:05:43 +0200

    nakresli prasátko

--- src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
@@ -9,10 +9,67 @@ public class HlavniProgram {
     new HlavniProgram().start();
   }
 
+  public void nakresliDomecek() {
+    for (int i = 0; i < 4; i++) {
+      zofka.move(100);
+      zofka.turnRight(90);
+    }
+    zofka.penUp();
+    zofka.move(100);
+    zofka.penDown();
+    zofka.turnRight(45);
+    zofka.move(70);
+    zofka.turnRight(90);
+    zofka.move(70);
+  }
+    public void nakresliNozicky() {
+      zofka.penDown();
+      zofka.turnRight(45);
+      zofka.move(30);
+      zofka.turnRight(180);
+      zofka.penUp();
+      zofka.move(30);
+      zofka.penDown();
+      zofka.turnRight(90);
+      zofka.move(30);
+      zofka.penUp();
+      zofka.turnLeft(180);
+      zofka.move(30);
+    }
+
+  public void nakresliPrasatko()  {
+    zofka.turnLeft(90);
+    nakresliDomecek();
+    zofka.turnRight(135);
+    zofka.penUp();
+    zofka.move(100);
+    nakresliNozicky();
+    zofka.turnRight(135);
+    zofka.move(100);
+    zofka.turnRight(90);
+    nakresliNozicky();
+    zofka.turnRight(45);
+    //ocasek
+    zofka.penUp();
+    zofka.move(100);
+    zofka.penDown();
+    zofka.turnRight(45);
+    zofka.move(20);
+  }
+
+  public void nakresliKruh() {
+    for (int i = 0; i < 72; i++) {
+      zofka.turnRight(5);
+      zofka.move(4);
+    }
+
+  }
+
+
   public void start() {
     zofka = new Turtle();
 
     //TODO implementace domácího úkolu
+    nakresliPrasatko();
   }
-
 }
